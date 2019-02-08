1ed0de0bef388d560c2b4084d9273bc3d8418e97
EvilTea <eviltea6991@gmail.com>
2019-02-08 21:06:50 +0200

    js-lesson new

---
+++ js1/main1.js
@@ -0,0 +1,56 @@
+var user, age, message, array , obj;
+
+var user = 'Denis', age = 21, message = 'Hello', array = [1, 2, 3], obj = {lastName: 'Titov'};
+
+console.log(user);
+console.log(age);
+console.log(message);
+console.log(array);
+console.log(obj);
+
+var a = 5e2;
+
+console.log(a);
+
+
+var i = 17;
+
+console.log(i);
+console.log(++i);
+console.log(i++);
+
+var n = 80;
+
+
+n += 10;
+console.log(n);
+
+
+n -= 20;
+console.log(n);
+
+
+n *= 2;
+console.log(n);
+
+
+n /= 4;
+console.log(n);
+
+
+n %= 30;
+console.log(n);
+
+
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random());
+console.log(Math.pow(2,5));
+
+
+
+console.log('Необычайно длиный текст'.length);
+console.log('Необычайно длиный текст'.substring(12));
+console.log('Необычайно длиный текст'.replace('длиный', 'короткий'));
+console.log('Необычайно длиный текст'.toUpperCase());
+console.log('Необычайно длиный текст'.toLowerCase());
